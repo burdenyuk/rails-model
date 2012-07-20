df20bf0f04476c08c451eac7c52bbbcd66590600
Denis Burdenyuk <burdenyukdenis@gmail.com>
2012-07-20 12:27:30 +0300

    Code convention fix

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,12 @@
 class Post < ActiveRecord::Base
   attr_accessible :title, :message
 
-  #validates :title, :presence => true
-  validates :message, :presence => true, :if => ":title.blank?"
+  validates :message, presence: true, if: :check_title
 
   before_validation :check_title
 
   protected
+
   def check_title
     self.title = "default" if self.title.blank?
   end
